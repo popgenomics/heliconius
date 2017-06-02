1634ca6840030ab75c0a84b4edeaaa40b383024b
popgenomics <tauraduboudin@gmail.com>
2017-06-02 15:03:17 +0100

    prepare input files for the locus specific ABC model comparison under the SC model

--- scripts/ABC_monolocus_SC.py
+++ scripts/ABC_monolocus_SC.py
@@ -26,18 +26,93 @@ for i in infile:
 		posterior[j].append(float(i[j]))
 infile.close()
 
-
-os.system("mkdir migration")
+# M1: migration in species 1 (M1!=0; M2=0)
+os.system("mkdir M1")
 
 spinput = "\n{0}\n{1}\n{2}\n{3}\n{4}\nmyfifo".format(1, nsamA, nsamB, L, nRep)
 
 for sub_rep in range(10):
-	os.system("mkdir migration/rep_{0}".format(sub_rep))
-	outfile = open("migration/rep_{0}/spinput.txt".format(sub_rep), "w")
+	os.system("mkdir M1/rep_{0}".format(sub_rep))
+	outfile = open("M1/rep_{0}/spinput.txt".format(sub_rep), "w")
 	outfile.write(spinput)
 	outfile.close()
 	
-	outfile = open("migration/rep_{0}/prior_monolocus_SC_migration_{0}.txt".format(sub_rep), "w")
+	outfile = open("M1/rep_{0}/prior.txt".format(sub_rep), "w")
+	nPosterior = len(posterior[0])
+	for i in range(nRep):
+		setOfParam = choice(nPosterior)
+		N1 = posterior[0][setOfParam] * beta(posterior[9][setOfParam], posterior[10][setOfParam])
+		N1past = N1 * posterior[1][setOfParam]
+		tauN1past = posterior[2][setOfParam]
+		
+		N2 = posterior[3][setOfParam] * beta(posterior[9][setOfParam], posterior[10][setOfParam])
+		N2past = N2 * posterior[4][setOfParam]
+		tauN2past = posterior[5][setOfParam]
+		
+		Na = posterior[6][setOfParam] * beta(posterior[9][setOfParam], posterior[10][setOfParam])
+		
+		Tsplit = posterior[7][setOfParam]
+		
+		Tsc = posterior[8][setOfParam]
+		
+		M1 = posterior[11][setOfParam] * beta(posterior[13][setOfParam], posterior[14][setOfParam])
+		M2 = 0 #posterior[12][setOfParam] * beta(posterior[15][setOfParam], posterior[16][setOfParam])
+		
+		# tbs nRep -t tbs -r tbs tbs -I 2 tbs tbs -m 1 2 tbs -m 2 1 tbs -n 1 tbs -n 2 tbs -en tbs 1 tbs -en tbs 2 tbs -eM tbs 0 -ej tbs 2 1 -eN tbs tbs
+		res = "\t".join([ str(k) for k in [nsamA+nsamB, theta, rho, L, nsamA, nsamB, M1, M2, N1, N2, tauN1past, N1past, tauN2past, N2past, Tsc, Tsplit, Tsplit, Na] ]) + "\n"
+		outfile.write(res)
+	outfile.close()
+
+
+# M2: migration in species 2 (M2!=0; M1=0)
+os.system("mkdir M2")
+
+spinput = "\n{0}\n{1}\n{2}\n{3}\n{4}\nmyfifo".format(1, nsamA, nsamB, L, nRep)
+
+for sub_rep in range(10):
+	os.system("mkdir M2/rep_{0}".format(sub_rep))
+	outfile = open("M2/rep_{0}/spinput.txt".format(sub_rep), "w")
+	outfile.write(spinput)
+	outfile.close()
+	
+	outfile = open("M2/rep_{0}/prior.txt".format(sub_rep), "w")
+	nPosterior = len(posterior[0])
+	for i in range(nRep):
+		setOfParam = choice(nPosterior)
+		N1 = posterior[0][setOfParam] * beta(posterior[9][setOfParam], posterior[10][setOfParam])
+		N1past = N1 * posterior[1][setOfParam]
+		tauN1past = posterior[2][setOfParam]
+		
+		N2 = posterior[3][setOfParam] * beta(posterior[9][setOfParam], posterior[10][setOfParam])
+		N2past = N2 * posterior[4][setOfParam]
+		tauN2past = posterior[5][setOfParam]
+		
+		Na = posterior[6][setOfParam] * beta(posterior[9][setOfParam], posterior[10][setOfParam])
+		
+		Tsplit = posterior[7][setOfParam]
+		
+		Tsc = posterior[8][setOfParam]
+		
+		M1 = 0 # posterior[11][setOfParam] * beta(posterior[13][setOfParam], posterior[14][setOfParam])
+		M2 = posterior[12][setOfParam] * beta(posterior[15][setOfParam], posterior[16][setOfParam])
+		
+		# tbs nRep -t tbs -r tbs tbs -I 2 tbs tbs -m 1 2 tbs -m 2 1 tbs -n 1 tbs -n 2 tbs -en tbs 1 tbs -en tbs 2 tbs -eM tbs 0 -ej tbs 2 1 -eN tbs tbs
+		res = "\t".join([ str(k) for k in [nsamA+nsamB, theta, rho, L, nsamA, nsamB, M1, M2, N1, N2, tauN1past, N1past, tauN2past, N2past, Tsc, Tsplit, Tsplit, Na] ]) + "\n"
+		outfile.write(res)
+	outfile.close()
+
+# M3: migration in both directions, asymetric
+os.system("mkdir M3")
+
+spinput = "\n{0}\n{1}\n{2}\n{3}\n{4}\nmyfifo".format(1, nsamA, nsamB, L, nRep)
+
+for sub_rep in range(10):
+	os.system("mkdir M3/rep_{0}".format(sub_rep))
+	outfile = open("M3/rep_{0}/spinput.txt".format(sub_rep), "w")
+	outfile.write(spinput)
+	outfile.close()
+	
+	outfile = open("M3/rep_{0}/prior.txt".format(sub_rep), "w")
 	nPosterior = len(posterior[0])
 	for i in range(nRep):
 		setOfParam = choice(nPosterior)
@@ -64,14 +139,15 @@ for sub_rep in range(10):
 	outfile.close()
 
 
-os.system("mkdir isolation")
+# M0: no migration
+os.system("mkdir M0")
 for sub_rep in range(10):
-	os.system("mkdir isolation/rep_{0}".format(sub_rep))
-	outfile = open("isolation/rep_{0}/spinput.txt".format(sub_rep), "w")
+	os.system("mkdir M0/rep_{0}".format(sub_rep))
+	outfile = open("M0/rep_{0}/spinput.txt".format(sub_rep), "w")
 	outfile.write(spinput)
 	outfile.close()
 	
-	outfile = open("isolation/rep_{0}/prior_monolocus_SC_isolation_{0}.txt".format(sub_rep), "w")
+	outfile = open("M0/rep_{0}/prior.txt".format(sub_rep), "w")
 	nPosterior = len(posterior[0])
 	for i in range(nRep):
 		setOfParam = choice(nPosterior)
@@ -98,7 +174,7 @@ for sub_rep in range(10):
 	outfile.close()
 
 
-print('bsub -q normal <<<"mknod myfifo p; mscalc < myfifo & msnsam tbs {0} -t tbs -r tbs tbs -I 2 tbs tbs 0 -m 1 2 tbs -m 2 1 tbs -n 1 tbs -en tbs 1 tbs -n 2 tbs -en tbs 2 tbs -eM tbs 0 -ej tbs 2 1 -eN tbs tbs >myfifo"'.format(nRep))
+print('bsub -q normal <<<"mknod myfifo p; mscalc < myfifo & msnsam tbs {0} -t tbs -r tbs tbs -I 2 tbs tbs 0 -m 1 2 tbs -m 2 1 tbs -n 1 tbs -en tbs 1 tbs -n 2 tbs -en tbs 2 tbs -eM tbs 0 -ej tbs 2 1 -eN tbs tbs <prior.txt >myfifo"'.format(nRep))
 
 
 
